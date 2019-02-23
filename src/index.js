32cb99ad3f27022057db2041c2b8430b2501b914
Katsiaryna Kananchuk <katekononchyk@gmail.com>
2019-02-24 00:44:46 +0300

    debug problem

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   
-
   const openBrackets = [];
   const closeBrackets = [];
+
   const arrStr = str.split('');
+  if (arrStr.length % 2 == 1) {
+    return false
+  } else {
+  
   for (let i = 0; i < bracketsConfig.length; i++) {
     openBrackets.push(bracketsConfig[i][0]);
     closeBrackets.push(bracketsConfig[i][1]);
@@ -12,28 +16,43 @@ module.exports = function check(str, bracketsConfig) {
   const stack = [];
   const arrIndex = [];
   
+  
   for (let i = 0; i < arrStr.length; i++) {
-    
-    if ( openBrackets.indexOf(arrStr[i]) != -1) {
+  
+    if (openBrackets.indexOf(arrStr[i]) != -1) {
+  
       var index = openBrackets.indexOf(arrStr[i]);
-        arrIndex.push(index);
-        stack.push(arrStr[i]);  
   
-        continue;
-      } 
-    if (arrStr[i] == closeBrackets[arrIndex[arrIndex.length-1]]) {
-      if (stack.length != 0) {
-        stack.pop();
-        arrIndex.pop();
+      arrIndex.push(index);
+      stack.push(arrStr[i]);
   
-        continue;
-        
-      } else return false
-    }  
+      if (stack[stack.length - 1] == stack[stack.length - 2]) {
+        if (openBrackets[index] == closeBrackets[index]) {
+
+          stack.pop();
+          arrIndex.pop();
+          stack.pop();
+          arrIndex.pop();
+        }
+      }
+  
+      continue;
+    }
+  
+  
+  if (arrStr[i] == closeBrackets[arrIndex[arrIndex.length - 1]]) {
+    if (stack.length != 0  ) {
+      stack.pop();
+      arrIndex.pop();
+      continue;
+    } else return false
+  } 
     
   }
   if (stack.length === 0) return true
-  else return false
+    else return false
+  } 
+  
   
   
 }
