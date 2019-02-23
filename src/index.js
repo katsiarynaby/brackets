eb524879216c86be58c0c9990dc7f2f6c6798a63
Katsiaryna Kananchuk <katekononchyk@gmail.com>
2019-02-23 23:42:27 +0300

    intermediate result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+
+  const openBrackets = [];
+  const closeBrackets = [];
+  const arrStr = str.split('');
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+  
+  const stack = [];
+  const arrIndex = [];
+  
+  for (let i = 0; i < arrStr.length; i++) {
+    
+    if ( openBrackets.indexOf(arrStr[i]) != -1) {
+      var index = openBrackets.indexOf(arrStr[i]);
+        arrIndex.push(index);
+        stack.push(arrStr[i]);  
+  
+        continue;
+      } 
+    if (arrStr[i] == closeBrackets[arrIndex[arrIndex.length-1]]) {
+      if (stack.length != 0) {
+        stack.pop();
+        arrIndex.pop();
+  
+        continue;
+        
+      } else return false
+    }  
+    
+  }
+  if (stack.length === 0) return true
+  else return false
+  
+  
 }
