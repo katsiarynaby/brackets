33852ebecd0581ddeb9d2fbc9b5cdb568a7a6bd0
Katsiaryna Kananchuk <katekononchyk@gmail.com>
2019-02-24 11:28:04 +0300

    add comments

--- src/index.js
+++ src/index.js
@@ -19,15 +19,16 @@ module.exports = function check(str, bracketsConfig) {
 
 
     for (let i = 0; i < arrStr.length; i++) {
-
-      if (openBrackets.indexOf(arrStr[i]) != -1) {
+      //можно indexof или includes
+      // если скобка открытая, то идем по этой ветке
+      if (openBrackets.includes(arrStr[i])) {
 
         var index = openBrackets.indexOf(arrStr[i]);
 
         arrIndex.push(index);
         stack.push(arrStr[i]);
 
-
+        // если одинаковые рядом
         if (stack[stack.length - 1] == stack[stack.length - 2]) {
           if (openBrackets[index] == closeBrackets[index]) {
 
@@ -40,21 +41,22 @@ module.exports = function check(str, bracketsConfig) {
         continue;
       }
 
+      // если первая закрытая
       if (!closeBrackets[arrIndex[arrIndex.length - 1]]) {
         return false
       }
-
+      // если после открытой идет такая же закрытая
       if (arrStr[i] == closeBrackets[arrIndex[arrIndex.length - 1]]) {
-
+      // и есть еще что удалять
         if (stack.length !== 0) {
           stack.pop();
           arrIndex.pop();
         } else return false
-
+//если после открытой идет закрытая, но не такая как нужна
       } else return false
 
     }
-
+//когда прошлись по всему и стек пусть
     if (stack.length === 0) return true
 
     else return false;
